ba8bba6e4f075d10f22528c59d62caee387915db
cod-panda <k.l.dvorkin@gmail.com>
2022-05-14 00:36:12 +0300

    main.tf for the helm

---
+++ helm/main.tf
@@ -0,0 +1,18 @@
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+resource "helm_release" "springdemo" {
+  name  = "springs"
+  chart = "./spring-kube"
+  cleanup_on_fail = true
+  timeout = 60
+  wait = false
+  lint = true
+}
